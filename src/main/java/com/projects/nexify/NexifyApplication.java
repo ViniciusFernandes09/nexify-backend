aedebbf27766c2f649cacdeff905c0a7edeb1ea1
Vinicius Fernandes <contato.viniciusfernandes9@gmail.com>
2026-08-06 21:12:27 -0300

    Order

---
+++ src/main/java/com/projects/nexify/NexifyApplication.java
@@ -0,0 +1,13 @@
+package com.projects.nexify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexifyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/projects/nexify/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.projects.nexify.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
