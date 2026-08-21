10df5bc5bc07d4522d8e828041be519d339c9dbf
Vinicius Fernandes <contato.viniciusfernandes9@gmail.com>
2026-08-21 17:59:51 -0300

    GET Products paged

--- src/main/java/com/projects/nexify/services/ProductService.java
+++ src/main/java/com/projects/nexify/services/ProductService.java
@@ -5,9 +5,12 @@ import com.projects.nexify.dto.ProductDTO;
 import com.projects.nexify.entities.Product;
 import com.projects.nexify.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,5 +25,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return  dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
