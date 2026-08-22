d1290a40df4202bb5b3472142cf6bf3b7ce499a0
Vinicius Fernandes <contato.viniciusfernandes9@gmail.com>
2026-08-22 03:21:29 -0300

    Bean Validation

--- src/main/java/com/projects/nexify/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/projects/nexify/controllers/handlers/ControllerExceptionHandler.java
@@ -1,12 +1,15 @@
 package com.projects.nexify.controllers.handlers;
 
 import com.projects.nexify.dto.CustomError;
+import com.projects.nexify.dto.ValidationError;
 import com.projects.nexify.services.exceptions.DatabaseException;
 import com.projects.nexify.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,6 +29,16 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados nválidos", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(err);
     }
 }
