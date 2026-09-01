04b71a94b6fc412b3e0d060aeca7589adc2c132c
Vinicius Fernandes <contato.viniciusfernandes9@gmail.com>
2026-08-31 23:51:08 -0300

    SearchByName products

--- src/main/java/com/projects/nexify/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/projects/nexify/controllers/handlers/ControllerExceptionHandler.java
@@ -35,7 +35,7 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<CustomError> methodArgumentNotValidation(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
-        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados nválidos", request.getRequestURI());
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
         for (FieldError f : e.getBindingResult().getFieldErrors()) {
             err.addError(f.getField(), f.getDefaultMessage());
         }
